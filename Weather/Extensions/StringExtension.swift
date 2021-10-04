47a042c4f385712bf89914d8e9ae8181cd820666
Dmitriy Nechaev <nechaytv@gmail.com>
2021-10-04 18:50:54 +0300

    Refactoring extensions

--- Weather/Extensions/StringExtension.swift
+++ Weather/Extensions/StringExtension.swift
@@ -8,11 +8,10 @@
 import Foundation
 
 extension String {
-    
     static func changeDateFormat(dateString: String, from inputFormat: String, to outputFormat: String) -> String {
         let inputDateFormatter = DateFormatter()
         inputDateFormatter.dateFormat = inputFormat
-        assert(inputDateFormatter.date(from: dateString) == nil)
+        assert(inputDateFormatter.date(from: dateString) != nil)
         let date = inputDateFormatter.date(from: dateString) ?? Date()
         
         let outputDateFormatter = DateFormatter()
