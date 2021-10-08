fa420c793cc4bb2ae27e06f5e4670b14cc4006d1
Dmitriy Nechaev <nechaytv@gmail.com>
2021-10-08 15:53:52 +0300

    Change the URL string in the network layer

--- Weather/Services/Network/NetworkLayer.swift
+++ Weather/Services/Network/NetworkLayer.swift
@@ -15,33 +15,36 @@ protocol NetworkWeatherServiceProtocol {
 class NetworkWeatherManager: NetworkWeatherServiceProtocol {
     
     func fetchCurrentWeather(forReqquesType requesType: HTTPRequestType, complitionHandler: @escaping (Result<CurrentWeatherData, Error>) -> Void) {
-        
-        var urlString = ""
-        
-        switch requesType {
-        case .city(city: let city):
-            urlString = "https://api.openweathermap.org/data/2.5/forecast?q=\(city.encodeUrl)&appid=6fbf251bda906b2794f2c3495a036ab3&cnt=40&units=metric&lang=ru"
-            print(urlString)
-            
-        case .coordinates(latitude: let latitude, longitude: let longitude):
-            urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=6fbf251bda906b2794f2c3495a036ab3&cnt=5&units=metric&lang=ru"
-        }
-        
-        guard let url = URL(string: urlString) else { return }
-        
+        guard let url = createURL(requesType: requesType) else { return }
         URLSession.shared.dataTask(with: url) { data, _, error in
             if let error = error {
                 complitionHandler(.failure(error))
                 return
             }
-            let decoder = JSONDecoder()
             do {
-                let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data!)
+                let currentWeatherData = try JSONDecoder().decode(CurrentWeatherData.self, from: data!)
                 complitionHandler(.success(currentWeatherData))
-                
             } catch {
                 complitionHandler(.failure(error))
             }
         }.resume()
+    }
+    
+    private func createURL(requesType: HTTPRequestType) -> URL? {
+        
+        let url = "https://api.openweathermap.org/data/2.5/forecast?"
+        let parametres = "&appid=6fbf251bda906b2794f2c3495a036ab3&cnt=40&units=metric&lang=ru"
+        var urlString = ""
+        
+        switch requesType {
+        case .city(city: let city):
+            urlString = url + "q=\(city.encodeUrl)" + parametres
+            
+        case .coordinates(latitude: let latitude, longitude: let longitude):
+            urlString = url + "lat=\(latitude)&lon=\(longitude)" + parametres
+        }
+        
+        guard let url = URL(string: urlString) else { return nil }
+        return url
     }
 }
