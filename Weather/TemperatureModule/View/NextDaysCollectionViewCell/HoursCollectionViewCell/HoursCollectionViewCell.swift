2d51d5b10a0eacc23f60735583d24521f876303a
Dmitriy Nechaev <nechaytv@gmail.com>
2021-10-08 10:55:15 +0300

    Make the HoursCollectionView layout into a separate class

--- Weather/TemperatureModule/View/NextDaysCollectionViewCell/HoursCollectionViewCell/HoursCollectionViewCell.swift
+++ Weather/TemperatureModule/View/NextDaysCollectionViewCell/HoursCollectionViewCell/HoursCollectionViewCell.swift
@@ -7,60 +7,18 @@
 
 import UIKit
 
+protocol HoursCollectionViewCellProtocol: UIView {
+    func setUpData(model: WeatherByHours)
+}
+
 final class HoursCollectionViewCell: UICollectionViewCell {
     
     static let id = "HoursCollectionViewCell"
-    
-    var hourLabel = UILabel()
-    var weatherImage = UIImageView()
-    var temperatureLabel = UILabel()
-    
-    func configure() {
-        setUpUI()
-        setUpConstraints()
-    }
+    var view: HoursCollectionViewCellProtocol!
     
     func setUpData(model: WeatherByHours) {
-        hourLabel.text = model.hour
-        weatherImage.image = UIImage(named: model.weatherImage) ?? #imageLiteral(resourceName: "01d")
-        temperatureLabel.text = model.temp
-    }
-    
-    private func setUpUI() {
-        hourLabel.text = "hour"
-        hourLabel.font = UIFont.medium(size: 16)
-        hourLabel.textAlignment = .center
-        hourLabel.adjustsFontSizeToFitWidth = true
-        hourLabel.minimumScaleFactor = 0.5
-        contentView.addSubview(hourLabel)
-        hourLabel.textColor = UIColor(named: "grayText")
-        weatherImage.image = #imageLiteral(resourceName: "01d")
-        
-        contentView.addSubview(weatherImage)
-        
-        temperatureLabel.text = "temp"
-        temperatureLabel.font = UIFont.extraBold(size: 16)
-        temperatureLabel.textAlignment = .center
-        contentView.addSubview(temperatureLabel)
-    }
-    
-    private func setUpConstraints() {
-        offAutoresizingMasks(elements: [hourLabel, weatherImage, temperatureLabel])
-        
-        NSLayoutConstraint.activate([
-            hourLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
-            hourLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            hourLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
-            weatherImage.topAnchor.constraint(equalTo: hourLabel.bottomAnchor, constant: 8),
-            weatherImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            weatherImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            weatherImage.heightAnchor.constraint(equalTo: weatherImage.widthAnchor, multiplier: 1.0/1.0),
-            
-            temperatureLabel.topAnchor.constraint(equalTo: weatherImage.bottomAnchor, constant: 8),
-            temperatureLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            temperatureLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            temperatureLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12)
-        ])
+        view = HoursCollectionViewScreen(frame: self.bounds)
+        view.setUpData(model: model)
+        contentView.addSubview(view)
     }
 }
---
+++ Weather/TemperatureModule/View/NextDaysCollectionViewCell/HoursCollectionViewCell/HoursCollectionViewScreen.swift
@@ -0,0 +1,64 @@
+//
+//  HoursCollectionViewScreen.swift
+//  Weather
+//
+//  Created by Дмитрий  on 08.10.2021.
+//
+
+import UIKit
+
+class HoursCollectionViewScreen: UIView, HoursCollectionViewCellProtocol {
+    
+    var hourLabel = UILabel()
+    var weatherImage = UIImageView()
+    var temperatureLabel = UILabel()
+    
+    func setUpData(model: WeatherByHours) {
+        setUpUI()
+        setUpConstraints()
+        hourLabel.text = model.hour
+        weatherImage.image = UIImage(named: model.weatherImage) ?? #imageLiteral(resourceName: "01d")
+        temperatureLabel.text = model.temp
+    }
+    
+    private func setUpUI() {
+        hourLabel.text = "hour"
+        hourLabel.font = UIFont.medium(size: 16)
+        hourLabel.textAlignment = .center
+        hourLabel.adjustsFontSizeToFitWidth = true
+        hourLabel.minimumScaleFactor = 0.5
+        
+        self.addSubview(hourLabel)
+        
+        hourLabel.textColor = UIColor(named: "grayText")
+        weatherImage.image = #imageLiteral(resourceName: "01d")
+        
+        self.addSubview(weatherImage)
+        
+        temperatureLabel.text = "temp"
+        temperatureLabel.font = UIFont.extraBold(size: 16)
+        temperatureLabel.textAlignment = .center
+        
+        self.addSubview(temperatureLabel)
+    }
+    
+    private func setUpConstraints() {
+        offAutoresizingMasks(elements: [hourLabel, weatherImage, temperatureLabel])
+        
+        NSLayoutConstraint.activate([
+            hourLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            hourLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            hourLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            weatherImage.topAnchor.constraint(equalTo: hourLabel.bottomAnchor, constant: 8),
+            weatherImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            weatherImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            weatherImage.heightAnchor.constraint(equalTo: weatherImage.widthAnchor, multiplier: 1.0/1.0),
+            
+            temperatureLabel.topAnchor.constraint(equalTo: weatherImage.bottomAnchor, constant: 8),
+            temperatureLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            temperatureLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            temperatureLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12)
+        ])
+    }
+}
