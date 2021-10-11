0b8cdc4e35653f4775663ec3f55e2e72fe5d517d
Dmitriy Nechaev <nechaytv@gmail.com>
2021-10-11 17:34:22 +0300

    Change the alert style to destructive

--- Weather/TemperatureModule/View/TemperatureViewController/Alerts/DarkModeErrorAlert.swift
+++ Weather/TemperatureModule/View/TemperatureViewController/Alerts/DarkModeErrorAlert.swift
@@ -11,7 +11,7 @@ final class DarkModeErrorAlert: UIAlertController, ErrorAlertProtocol {
     func show() {
         title = "iOS ниже 13"
         message = "Темная тема доступна только с версии iOS 13 и выше"
-        let alertAction = UIAlertAction(title: "Ок", style: .default, handler: nil)
+        let alertAction = UIAlertAction(title: "Ок", style: .destructive, handler: nil)
         if self.actions.count == 0 {
             self.addAction(alertAction)
         }
