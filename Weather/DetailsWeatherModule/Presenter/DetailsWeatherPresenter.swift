8eb54bb79fd87bea2fa119f8ac7c9dac38bf89db
Dmitriy Nechaev <nechaytv@gmail.com>
2021-10-20 21:08:32 +0300

    Create a sketch of the presenter

--- Weather/DetailsWeatherModule/Presenter/DetailsWeatherPresenter.swift
+++ Weather/DetailsWeatherModule/Presenter/DetailsWeatherPresenter.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 protocol DetailWeatherViewProtocol: AnyObject {
-    
+    func setWeatherData(data: WeatherDataProtocol)
 }
 
 protocol DetailsWeatherPresenterProtocol: AnyObject {
+    func showData()
+}
+
+final class DetailsWeatherPresenter: DetailsWeatherPresenterProtocol {
+    
+    weak var view: DetailWeatherViewProtocol!
+    private var weatherData: WeatherDataProtocol!
+    
+    init(view: DetailWeatherViewProtocol, weatherData: WeatherDataProtocol) {
+        self.view = view
+        self.weatherData = weatherData
+    }
+    
+    func showData() {
+    }
     
 }
