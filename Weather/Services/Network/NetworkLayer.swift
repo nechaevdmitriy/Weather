aee76abcad07cfe9ef35632b42b35c13696419b0
Dmitriy Nechaev <nechaytv@gmail.com>
2021-12-05 16:13:12 +0300

    Add UserDefault

--- Weather/Services/Network/NetworkLayer.swift
+++ Weather/Services/Network/NetworkLayer.swift
@@ -26,18 +26,26 @@ final class NetworkWeatherManager: NetworkWeatherServiceProtocol {
             
             switch dataResponse.result {
             case .success(let data):
+                UserDefaults.standard.set(try? PropertyListEncoder().encode(data), forKey: "weather")
                 complitionHandler(RequesResult.succes(value: data))
             case .failure(let error):
-                complitionHandler(RequesResult.failure(error: error))
+                if let data = UserDefaults.standard.value(forKey:"weather") as? Data {
+                    if let weatherData = try? PropertyListDecoder().decode(CurrentWeatherData.self, from: data) {
+                        complitionHandler(RequesResult.succes(value: weatherData))
+                    }
+                } else {
+                    complitionHandler(RequesResult.failure(error: error))
+                }
             }
         }
     }
-    
-    private func createURL() -> URL? {
-        let city = "Санкт-Петербург"
-        let urlString = "https://api.openweathermap.org/data/2.5/forecast?q=\(city.encodeUrl)&appid=6fbf251bda906b2794f2c3495a036ab3&cnt=40&units=metric&lang=ru"
-        
-        guard let url = URL(string: urlString) else { return nil }
-        return url
-    }
 }
+
+private func createURL() -> URL? {
+    let city = "Санкт-Петербург"
+    let urlString = "https://api.openweathermap.org/data/2.5/forecast?q=\(city.encodeUrl)&appid=6fbf251bda906b2794f2c3495a036ab3&cnt=40&units=metric&lang=ru"
+    
+    guard let url = URL(string: urlString) else { return nil }
+    return url
+}
+
